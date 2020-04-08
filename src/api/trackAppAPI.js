bdd1d6ec395faa966d93352e8c161e191685b2b5
Anthony Ellis <aellisnola@gmail.com>
2020-04-08 03:17:15 -0500

    Added link to navigate from Signup to Signin

--- src/api/trackAppAPI.js
+++ src/api/trackAppAPI.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 // baseURL only lasts 8 hours witn NGROK, so change often or errors will occur.
 export default axios.create({
-    baseURL: 'https://938ee25c.ngrok.io/'
+    baseURL: 'https://5295c2d7.ngrok.io/'
 })
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 
 import Spacer from '../components/styling/Spacer'
@@ -37,6 +37,12 @@ const SignupScreen = ({ navigation }) => {
             <Spacer>
                 <Button title="Sign Up" onPress={() => signup({ email, password })} />
             </Spacer>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>Already have an account? Sign in here.</Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -61,6 +67,10 @@ const styles = StyleSheet.create({
     error: {
         fontSize: 16,
         color: 'red',
+        textAlign: 'center'
+    },
+    link: {
+        color: 'blue',
         textAlign: 'center'
     }
 })
