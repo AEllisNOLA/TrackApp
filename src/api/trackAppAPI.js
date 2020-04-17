698edc4fecdefefd11a6ca80541abb154914bd4d
Anthony Ellis <AEllisNOLA@gmail.com>
2020-04-17 15:39:13 -0500

    Forgot to merge with master apparently.

--- src/api/trackAppAPI.js
+++ src/api/trackAppAPI.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 // baseURL only lasts 8 hours witn NGROK, so change often or errors will occur.
 export default axios.create({
-    baseURL: 'https://66b11bb6.ngrok.io/'
+    baseURL: 'https://66c605f8.ngrok.io/'
 })
